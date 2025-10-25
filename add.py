75d2511d7340f48dc26a3156bd2061ec648e15af
mehekhakittur <mehekhakittur@gmail.com>
2025-10-25 08:26:58 +0530

    Implement addition and subtraction in add.py
    Add basic arithmetic operations with error handling.

---
+++ add.py
@@ -0,0 +1,8 @@
+#math.py
+try:
+  a= float(input("Enter frist number:"))
+  b= float(input("Enter second number:"))
+  print(f"addtion:{a+b}")
+  print(f"Subtraction:{a-b}")
+except valueerror:
+  print("please enter valid number.")
